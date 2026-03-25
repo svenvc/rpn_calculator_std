5a73fb8219065df8853d8e779c9b044bb96dc4c5
Sven Van Caekenberghe <sven@stfx.eu>
2026-03-25 10:54:58 +0100

    simplify dockerfile

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RPNCalculator.MixProject do
   def project do
     [
       app: :rpn_calculator,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
