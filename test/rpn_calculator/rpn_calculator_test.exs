804118a798f6bc7c5f83b4fbf18a94d59135fac9
Sven Van Caekenberghe <sven@stfx.eu>
2026-04-05 11:42:28 +0200

    increase test coverage of core rpn calculator model

--- test/rpn_calculator/rpn_calculator_test.exs
+++ test/rpn_calculator/rpn_calculator_test.exs
@@ -112,4 +112,59 @@ defmodule RPNCalculator.RPNCalculatorTest do
     rpn_calculator = rpn_calculator |> RPNCalculator.process_key("Drop")
     assert RPNCalculator.top_of_stack(rpn_calculator) == 0
   end
+
+  test "constants pi & e" do
+    rpn_calculator = %RPNCalculator{}
+    rpn_calculator = rpn_calculator |> RPNCalculator.process_key("Pi")
+    assert RPNCalculator.top_of_stack(rpn_calculator) == :math.pi()
+    rpn_calculator = rpn_calculator |> RPNCalculator.process_key("E")
+    assert RPNCalculator.top_of_stack(rpn_calculator) == :math.exp(1)
+  end
+
+  test "np-op, need 2 args" do
+    rpn_calculator = %RPNCalculator{}
+    rpn_calculator = rpn_calculator |> RPNCalculator.push(123)
+    ~w(Add Subtract Multiply Divide Power XY) |> Enum.each(fn key ->
+      rpn_calculator = rpn_calculator |> RPNCalculator.process_key(key)
+      assert RPNCalculator.top_of_stack(rpn_calculator) == 123
+    end)
+  end
+
+  test "inverses" do
+    rpn_calculator = %RPNCalculator{}
+    rpn_calculator = rpn_calculator |> RPNCalculator.push(123)
+    rpn_calculator = rpn_calculator |> RPNCalculator.process_keys(~w(Square Sqrt))
+    assert RPNCalculator.top_of_stack(rpn_calculator) == 123
+    rpn_calculator = rpn_calculator |> RPNCalculator.process_keys(~w(Exp Ln))
+    assert RPNCalculator.top_of_stack(rpn_calculator) == 123
+    rpn_calculator = rpn_calculator |> RPNCalculator.push(10)
+    rpn_calculator = rpn_calculator |> RPNCalculator.process_keys(~w(Power Log))
+    assert RPNCalculator.top_of_stack(rpn_calculator) == 123
+  end
+
+  test "render" do
+    assert RPNCalculator.render_number(123) == "123"
+    assert RPNCalculator.render_number(123.0) == "123"
+    assert RPNCalculator.render_number(123.5) == "123.5"
+    assert RPNCalculator.render_number(-123) == "-123"
+    assert RPNCalculator.render_number(-123.0) == "-123"
+    assert RPNCalculator.render_number(-123.5) == "-123.5"
+    assert RPNCalculator.render_number(1.23e5) == "123000"
+    assert RPNCalculator.render_number(-1.23e-5) == "-1.23e-5"
+    assert RPNCalculator.render_number(-1.234567890123456789) == "-1.23456789e+00"
+    assert RPNCalculator.render_number(1/3) == "0.3333333333333333"
+    assert RPNCalculator.render_number(-1/3) == "-3.33333333e-01"
+  end
+
+  test "edge cases input editing" do
+    rpn_calculator = %RPNCalculator{}
+    rpn_calculator = rpn_calculator |> RPNCalculator.process_keys(~w(0 1 2 3))
+    assert RPNCalculator.top_of_stack(rpn_calculator) == 123
+    rpn_calculator = rpn_calculator |> RPNCalculator.process_keys(~w(Clear Dot 5))
+    assert RPNCalculator.top_of_stack(rpn_calculator) == 0.5
+    rpn_calculator = rpn_calculator |> RPNCalculator.process_keys(~w(Dot 5))
+    assert RPNCalculator.top_of_stack(rpn_calculator) == 0.55
+    rpn_calculator = rpn_calculator |> RPNCalculator.process_keys(~w(Clear 1 Sign Dot 5 EE EE 4 Sign 4))
+    assert RPNCalculator.top_of_stack(rpn_calculator) == -1.5e-44
+  end
 end
