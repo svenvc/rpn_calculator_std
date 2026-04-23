0d9a2f705f20065b3102747a049ae84bc145ea5e
Sven Van Caekenberghe <sven@stfx.eu>
2026-04-23 15:05:19 +0200

    improve rest api error handling

--- lib/rpn_calculator_web/controllers/calculator_controller.ex
+++ lib/rpn_calculator_web/controllers/calculator_controller.ex
@@ -4,44 +4,54 @@ defmodule RPNCalculatorWeb.CalculatorController do
   alias RPNCalculator.RPNCalculator
 
   def new(conn, _params) do
-    data = %RPNCalculator{} |> Map.take([:rpn_stack, :input_digits, :computed?])
-    json(conn, data)
+    conn |> json(to_map(%RPNCalculator{}))
   end
 
   def known_keys(conn, _params) do
-    data = RPNCalculator.known_keys()
-    json(conn, data)
+    conn |> json(RPNCalculator.known_keys())
   end
 
-  def process_key(conn, %{
-        "input_digits" => input_digits,
-        "computed?" => computed?,
-        "rpn_stack" => rpn_stack,
-        "key" => key
-      } = _params)
-      when is_binary(input_digits) and
-             is_boolean(computed?) and
-             is_list(rpn_stack) and
-             is_binary(key) do
-    state = %RPNCalculator{input_digits: input_digits, computed?: computed?, rpn_stack: rpn_stack}
-    IO.puts("/process-key/#{key} - #{inspect(state)}")
-    state = state |> RPNCalculator.process_key(key)
-    data = state |> Map.take([:rpn_stack, :input_digits, :computed?])
-    json(conn, data)
+  def process_key(conn, %{"key" => key} = params)
+      when is_binary(key) do
+    try do
+      state = to_rpn_calculator(params)
+      IO.puts("/process-key/#{key} - #{inspect(state)}")
+      state = state |> RPNCalculator.process_key(key)
+      conn |> json(to_map(state))
+    rescue
+      error -> conn |> put_status(:bad_request) |> json(%{"error" => Exception.message(error)})
+    end
   end
 
-  def push(conn, %{
-        "input_digits" => input_digits,
-        "computed?" => computed?,
-        "rpn_stack" => rpn_stack,
-        "number" => number
-      } = _params)
+  def push(conn, %{"number" => number} = params)
       when is_binary(number) do
-    state = %RPNCalculator{input_digits: input_digits, computed?: computed?, rpn_stack: rpn_stack}
-    number = RPNCalculator.parse_input(number)
-    IO.puts("/push/#{number} - #{inspect(state)}")
-    state = state |> RPNCalculator.push(number)
-    data = state |> Map.take([:rpn_stack, :input_digits, :computed?])
-    json(conn, data)
+    try do
+      state = to_rpn_calculator(params)
+      number = RPNCalculator.parse_input(number)
+      IO.puts("/push/#{number} - #{inspect(state)}")
+      state = state |> RPNCalculator.push(number)
+      conn |> json(to_map(state))
+    rescue
+      error -> conn |> put_status(:bad_request) |> json(%{"error" => Exception.message(error)})
+    end
+  end
+
+  defp to_map(%RPNCalculator{} = rpn_calculator) do
+    rpn_calculator |> Map.take([:rpn_stack, :input_digits, :computed?])
+  end
+
+  defp to_rpn_calculator(%{
+         "input_digits" => input_digits,
+         "computed?" => computed?,
+         "rpn_stack" => rpn_stack
+       })
+       when is_binary(input_digits) and
+              is_boolean(computed?) and
+              is_list(rpn_stack) do
+    if !Enum.all?(rpn_stack, fn x -> is_number(x) end) do
+      raise "rpn_stack must only contain numbers"
+    end
+
+    %RPNCalculator{input_digits: input_digits, computed?: computed?, rpn_stack: rpn_stack}
   end
 end
